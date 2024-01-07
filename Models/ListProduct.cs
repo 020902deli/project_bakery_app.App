138c588a79a547cc480ef801769eb8e3e1d221d4
DELIA DUMITRU <delia.dumitru1@stud.ubbcluj.ro>
2024-01-07 13:31:59 +0200

    Am creat clasele pt tipurile de produse/prajituri

---
+++ Models/ListProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+
+namespace project_bakery_app.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(OrderList))]
+        public int OrderListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
