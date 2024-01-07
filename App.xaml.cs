7138d9e2030b757c79a2bddff4dccc01c9342d6b
DELIA DUMITRU <delia.dumitru1@stud.ubbcluj.ro>
2024-01-07 12:56:24 +0200

    Creat legatura pentru baza de date

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace project_bakery_app;
+﻿using System;
+using project_bakery_app.Data;
+using System.IO;
+namespace project_bakery_app;
 
 public partial class App : Application
 {
-	public App()
+    static OrderListDatabase database;
+    public static OrderListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               OrderListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "OrderList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
